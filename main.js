dc4aaa1b472f4c69f1556bd556b8d35b74db943c
IshanTheLegend <110982985+IshanTheLegend@users.noreply.github.com>
2022-08-10 20:18:23 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function change(){
+document.getElementById("football").src="https://fas-org-sg.s3.ap-southeast-1.amazonaws.com/wp-content/uploads/2022/06/15160929/2022-06-14-ACQ-Day-16-MYA-2-6-SIN-45-2.jpg";
+
+
+}
